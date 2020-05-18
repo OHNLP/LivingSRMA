8067a0d13876fcf8d64d2714d6902598f179897c
Huan He <hehuan2112@gmail.com>
2020-05-18 10:33:21 -0500

    add default user

--- schema.sql
+++ schema.sql
@@ -143,6 +143,7 @@ CREATE TABLE `users` (
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES ('hehuan2112@gmail.com','pbkdf2:sha256:150000$oHLnajng$9be224b50b962de1d83cd745124fc6fe6828e5079c560fa6654d668439b77503','Huan','He','user','no');
+INSERT INTO `users` VALUES ('sipra.irbaz@gmail.com','pbkdf2:sha256:150000$oHLnajng$9be224b50b962de1d83cd745124fc6fe6828e5079c560fa6654d668439b77503','Irbaz','Riaz','user','no');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
