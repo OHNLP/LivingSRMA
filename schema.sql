fe99a5ae56e03806342a2bb223f3410083eb5ac8
Huan He <hehuan2112@gmail.com>
2020-05-19 15:13:52 -0500

    update schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `lnma` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `lnma`;
 -- MySQL dump 10.13  Distrib 8.0.20, for macos10.15 (x86_64)
 --
 -- Host: 172.22.15.10    Database: lnma
@@ -200,4 +202,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-19 15:04:28
+-- Dump completed on 2020-05-19 15:13:19
