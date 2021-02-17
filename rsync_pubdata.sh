38136d4c781fed975ff9ac5f2d8df0e80a1f5bc6
Huan He <hehuan2112@gmail.com>
2021-02-16 20:30:07 -0600

    fix typoe

--- rsync_pubdata.sh
+++ rsync_pubdata.sh
@@ -5,7 +5,7 @@ if test -z "$PRJ"; then
     echo "* project short name is needed!"
     exit 1
 else
-    echo "* prepare the standalone webpage for $PRJ"
+    echo "* uploading the data file for $PRJ"
 fi
 
 # scp -r instance/pubdata/$PRJ/* lnmasrv:/home/m210842/sites/lnma/instance/pubdata/$PRJ/
