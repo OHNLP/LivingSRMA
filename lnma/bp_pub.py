a4373e161e33a38af9f50bed0aa46db62d6cc383
Huan He <hehuan2112@gmail.com>
2021-04-14 16:06:24 -0500

    fix study type text

--- lnma/bp_pub.py
+++ lnma/bp_pub.py
@@ -1464,12 +1464,12 @@ def get_itable_attr_rs_cfg_from_db(prj):
         for attr in basic_attrs:
             attr_name = "%s" % attr['name']
             if attr['abbr'] == 'ba_year':
-                val = util.get_year(paper.pub_date)
+                val = paper.get_year()
             elif attr['abbr'] == 'ba_authors':
                 # the author name maybe different format, use paper instead of `r`
                 val = util.get_author_etal_from_paper(paper)
             elif attr['abbr'] == 'ba_ofu':
-                val = paper.meta['study_type']
+                val = paper.get_study_type()
             elif attr['abbr'] == 'ba_pmid':
                 val = paper.pid
             elif attr['abbr'] == 'ba_nct':
