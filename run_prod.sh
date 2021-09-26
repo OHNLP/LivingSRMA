7fe985fcd82aac0affbcf91c380f71e77b67345b
Huan He <hehuan2112@gmail.com>
2021-09-25 21:03:58 -0500

    add outcome export. extend timeout

--- run_prod.sh
+++ run_prod.sh
@@ -2,9 +2,13 @@
 
 export FLASK_APP=lnma
 export FLASK_ENV=production
+export GUNICORN_TIMEOUT=120
 
 # create the tmp folder for files
 mkdir -p /dev/shm/lnma
 
 # run!
-gunicorn -b localhost:60088 -w 4 lnma.wsgi:app
+gunicorn \
+    -b localhost:60088 \
+    --timeout $GUNICORN_TIMEOUT \
+    -w 4 lnma.wsgi:app
