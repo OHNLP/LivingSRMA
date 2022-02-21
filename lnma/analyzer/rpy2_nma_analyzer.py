e3620b1a7c11b585f2d74e65ea76dcb2bee65a43
Huan He <hehuan2112@gmail.com>
2022-02-21 03:09:03 +0000

    update test and demo for nma

--- lnma/analyzer/rpy2_nma_analyzer.py
+++ lnma/analyzer/rpy2_nma_analyzer.py
@@ -32,85 +32,6 @@ meta = importr('meta')
 netmeta = importr('netmeta')
 jsonlite = importr('jsonlite')
 
-def demo():
-    '''
-    A demo case for showing how to use
-    '''
-    cfg = {
-        'which_is_better': 'lower'
-    }
-    df = pd.DataFrame(
-        [
-            ['Pembro', 'Placebo', 0.68, 0.53, 0.87, 'S1', 2021],
-            ['Suni', 'Placebo', 0.76, 0.59, 0.98, 'S2', 2016],
-            ['Sora', 'Placebo', 0.97, 0.80, 1.17, 'S3', 2016],
-            ['Pazo', 'Placebo', 0.84, 0.71, 0.99, 'S4', 2017],
-            ['Axi', 'Placebo', 0.87, 0.66, 1.15, 'S5', 2018]
-        ], 
-        columns=['t1', 't2', 'sm','lowerci', 'upperci','study','year']
-    )
-
-    # convert the TE
-    df['TE'] = np.log(df['sm'])
-
-    # usually the SE is not in the data
-    if 'seTE' not in df.columns:
-        # need to convert the SE
-        df['seTE'] = (np.log(df['upperci']) - np.log(df['lowerci'])) / 3.92
-
-    # get the primary
-    r_nma = netmeta.netmeta(
-        TE = df.TE,
-        seTE = df.seTE,
-        treat1 = df.t1,
-        treat2 = df.t2,
-        studlab = df.study,
-        sm = 'HR',
-        comb_random = False,
-        reference_group = 'Placebo'
-    )
-    r_j_nma = jsonlite.toJSON(r_nma, force=True)
-    j_nma = json.loads(r_j_nma[0])
-
-    # the network plot
-    r_netplt = netmeta.netgraph(r_nma)
-    r_j_netplt = jsonlite.toJSON(r_netplt, force=True)
-    j_netplt = json.loads(r_j_netplt[0])
-
-    # the league table
-    r_lgtb = netmeta.netleague(r_nma, bracket="(", digits=2)
-    r_j_lgtb = jsonlite.toJSON(r_lgtb, force=True)
-    j_lgtb = json.loads(r_j_lgtb[0])
-
-    # the forest
-    # r_forestplt = netmeta.forest(r_nma)
-    # r_j_forestplt = jsonlite.toJSON(r_forestplt, force=True)
-    # j_forestplt = json.loads(r_j_forestplt[0])
-
-    # the rank
-    r_rank = netmeta.netrank(
-        r_nma, 
-        small_values = 'good' if cfg['which_is_better'] == 'lower' else 'bad'
-    )
-    r_j_rank = jsonlite.toJSON(r_rank, force=True)
-    j_rank = json.loads(r_j_rank[0])
-
-
-    ret = {
-        'submission_id': 'rpy2',
-        'params': cfg,
-        'success': True,
-        'data': {
-            'forest': j_lgtb,
-            'league': j_lgtb,
-            'netcha': j_netplt,
-            'netplt': j_netplt,
-            'psrank': j_rank
-        }
-    }
-
-    pprint(ret)
-
 
 def analyze_nma_freq_pre(rs, cfg):
     '''
@@ -247,6 +168,111 @@ def analyze(rs, cfg):
                 ret = analyze_nma_freq_raw(rs, cfg)
 
     return ret
+
+
+def test():
+    '''
+    Just for test function
+    '''
+    cfg = {
+        'which_is_better': 'lower',
+        'measure_of_effect': 'HR',
+        'fixed_or_random': 'random'
+    }
+    rs = pd.DataFrame(
+        [
+            ['Pembro', 'Placebo', 0.68, 0.53, 0.87, 'S1', 2021],
+            ['Suni', 'Placebo', 0.76, 0.59, 0.98, 'S2', 2016],
+            ['Sora', 'Placebo', 0.97, 0.80, 1.17, 'S3', 2016],
+            ['Pazo', 'Placebo', 0.84, 0.71, 0.99, 'S4', 2017],
+            ['Axi', 'Placebo', 0.87, 0.66, 1.15, 'S5', 2018]
+        ], 
+        columns=['t1', 't2', 'sm','lowerci', 'upperci','study','year']
+    ).to_dict(orient='records')
+
+    ret = analyze_nma_freq_pre(rs, cfg)
+
+    pprint(ret)
+
+
+def demo():
+    '''
+    A demo case for showing how to use rpy2 for NMA
+    '''
+    cfg = {
+        'which_is_better': 'lower'
+    }
+    df = pd.DataFrame(
+        [
+            ['Pembro', 'Placebo', 0.68, 0.53, 0.87, 'S1', 2021],
+            ['Suni', 'Placebo', 0.76, 0.59, 0.98, 'S2', 2016],
+            ['Sora', 'Placebo', 0.97, 0.80, 1.17, 'S3', 2016],
+            ['Pazo', 'Placebo', 0.84, 0.71, 0.99, 'S4', 2017],
+            ['Axi', 'Placebo', 0.87, 0.66, 1.15, 'S5', 2018]
+        ], 
+        columns=['t1', 't2', 'sm','lowerci', 'upperci','study','year']
+    )
+
+    # convert the TE
+    df['TE'] = np.log(df['sm'])
+
+    # usually the SE is not in the data
+    if 'seTE' not in df.columns:
+        # need to convert the SE
+        df['seTE'] = (np.log(df['upperci']) - np.log(df['lowerci'])) / 3.92
+
+    # get the primary
+    r_nma = netmeta.netmeta(
+        TE = df.TE,
+        seTE = df.seTE,
+        treat1 = df.t1,
+        treat2 = df.t2,
+        studlab = df.study,
+        sm = 'HR',
+        comb_random = False,
+        reference_group = 'Placebo'
+    )
+    r_j_nma = jsonlite.toJSON(r_nma, force=True)
+    j_nma = json.loads(r_j_nma[0])
+
+    # the network plot
+    r_netplt = netmeta.netgraph(r_nma)
+    r_j_netplt = jsonlite.toJSON(r_netplt, force=True)
+    j_netplt = json.loads(r_j_netplt[0])
+
+    # the league table
+    r_lgtb = netmeta.netleague(r_nma, bracket="(", digits=2)
+    r_j_lgtb = jsonlite.toJSON(r_lgtb, force=True)
+    j_lgtb = json.loads(r_j_lgtb[0])
+
+    # the forest
+    r_forestplt = netmeta.forest_netmeta(r_nma)
+    r_j_forestplt = jsonlite.toJSON(r_forestplt, force=True)
+    j_forestplt = json.loads(r_j_forestplt[0])
+
+    # the rank
+    r_rank = netmeta.netrank(
+        r_nma, 
+        small_values = 'good' if cfg['which_is_better'] == 'lower' else 'bad'
+    )
+    r_j_rank = jsonlite.toJSON(r_rank, force=True)
+    j_rank = json.loads(r_j_rank[0])
+
+
+    ret = {
+        'submission_id': 'rpy2',
+        'params': cfg,
+        'success': True,
+        'data': {
+            'forest': j_lgtb,
+            'league': j_lgtb,
+            'netcha': j_netplt,
+            'netplt': j_netplt,
+            'psrank': j_rank
+        }
+    }
+
+    pprint(ret)
 
 
 if __name__ == '__main__':
