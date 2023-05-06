75cefeab03ec575cc9fe9f4d5638f0d5612472e7
Huan He <hehuan2112@gmail.com>
2023-05-06 03:08:02 +0000

    update schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,13 @@
-CREATE DATABASE  IF NOT EXISTS `lnma` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `lnma`;
--- MySQL dump 10.13  Distrib 8.0.20, for macos10.15 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
 --
--- Host: 172.22.15.10    Database: lnma
+-- Host: localhost    Database: lnma
 -- ------------------------------------------------------
--- Server version	8.0.19
+-- Server version	8.0.20
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -18,31 +16,54 @@ USE `lnma`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `admins`
+-- Table structure for table `alembic_version`
 --
 
-DROP TABLE IF EXISTS `admins`;
+DROP TABLE IF EXISTS `alembic_version`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `admins` (
-  `uid` varchar(64) NOT NULL,
-  `password` varchar(128) DEFAULT NULL,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `role` varchar(16) DEFAULT NULL,
-  PRIMARY KEY (`uid`)
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL,
+  PRIMARY KEY (`version_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `admins`
+-- Table structure for table `datasources`
 --
 
-LOCK TABLES `admins` WRITE;
-/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES ('hehuan','pbkdf2:sha256:150000$oHLnajng$9be224b50b962de1d83cd745124fc6fe6828e5079c560fa6654d668439b77503','Huan','He','root');
-/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `datasources`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `datasources` (
+  `datasource_id` varchar(48) NOT NULL,
+  `ds_type` varchar(48) DEFAULT NULL,
+  `title` text,
+  `content` longtext,
+  `date_created` datetime DEFAULT NULL,
+  PRIMARY KEY (`datasource_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `extracts`
+--
+
+DROP TABLE IF EXISTS `extracts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `extracts` (
+  `extract_id` varchar(48) NOT NULL,
+  `project_id` varchar(48) DEFAULT NULL,
+  `oc_type` varchar(48) DEFAULT NULL,
+  `abbr` varchar(48) DEFAULT NULL,
+  `meta` json DEFAULT NULL,
+  `data` json DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`extract_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `notes`
@@ -52,25 +73,16 @@ DROP TABLE IF EXISTS `notes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `notes` (
-  `note_id` varchar(48) NOT NULL COMMENT 'UUID',
-  `paper_id` varchar(48) NOT NULL,
-  `project_id` varchar(48) NOT NULL,
+  `note_id` varchar(48) NOT NULL,
+  `paper_id` varchar(48) DEFAULT NULL,
+  `project_id` varchar(48) DEFAULT NULL,
   `data` json DEFAULT NULL,
-  `date_created` datetime NOT NULL,
-  `date_updated` datetime NOT NULL,
-  `is_deleted` varchar(8) NOT NULL DEFAULT 'no',
+  `date_created` datetime DEFAULT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  `is_deleted` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`note_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `notes`
---
-
-LOCK TABLES `notes` WRITE;
-/*!40000 ALTER TABLE `notes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `notes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `papers`
@@ -81,33 +93,45 @@ DROP TABLE IF EXISTS `papers`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `papers` (
   `paper_id` varchar(48) NOT NULL,
-  `pid` varchar(64) NOT NULL,
-  `pid_type` varchar(8) NOT NULL DEFAULT 'pmid',
-  `project_id` varchar(32) NOT NULL,
-  `title` varchar(256) NOT NULL,
+  `pid` varchar(64) DEFAULT NULL,
+  `pid_type` text,
+  `project_id` varchar(48) DEFAULT NULL,
+  `title` text,
+  `abstract` longtext,
   `pub_date` varchar(32) DEFAULT NULL,
   `authors` text,
-  `journal` varchar(128) DEFAULT NULL,
-  `meta` json DEFAULT NULL COMMENT 'Use this JSON object to store all the information about this study',
-  `ss_st` varchar(8) NOT NULL DEFAULT 'NA' COMMENT 'Screening State Start: for study screening, indicating where the study comes from: b10: batch import/search, b11: other, b12: other manual way, a10: automatic way, a11: auto search',
-  `ss_pr` varchar(8) NOT NULL DEFAULT 'NA' COMMENT 'Screening State Process: for study screening, indicating which process the study is in: p30: loaded meta info, p40: checked unique, p50: checked title, p60: checked full text, p70: checked sr, p80: checked ma',
-  `ss_rs` varchar(8) NOT NULL DEFAULT 'NA' COMMENT 'Screening State Result: for study screening, indicating what result for the study: e1: excluded for duplicate, e2: excluded for title, e13: excluded for full text, e4: excluded for updates, f1: included in sr, f2: included in ma, f3: included in sr and ma',
-  `date_created` datetime NOT NULL,
-  `date_updated` datetime NOT NULL,
-  `is_deleted` varchar(8) NOT NULL DEFAULT 'no',
+  `journal` text,
+  `meta` json DEFAULT NULL,
+  `ss_st` varchar(8) DEFAULT NULL,
+  `ss_pr` varchar(8) DEFAULT NULL,
+  `ss_rs` varchar(8) DEFAULT NULL,
+  `ss_ex` json DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  `is_deleted` varchar(8) DEFAULT NULL,
+  `seq_num` int DEFAULT NULL,
   PRIMARY KEY (`paper_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `papers`
+-- Table structure for table `pieces`
 --
 
-LOCK TABLES `papers` WRITE;
-/*!40000 ALTER TABLE `papers` DISABLE KEYS */;
-INSERT INTO `papers` VALUES ('3dbdd662-97f7-11ea-9a4b-98eecba7646f','24019545','pmid','123','','','','',NULL,'a12','na','na','2020-05-16 23:31:14','2020-05-16 23:31:14','no'),('64a46c7a-99e9-11ea-9a4b-98eecba7646f','24019545','pmid','123','','','','',NULL,'a12','na','na','2020-05-19 10:57:08','2020-05-19 10:57:08','no');
-/*!40000 ALTER TABLE `papers` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `pieces`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pieces` (
+  `piece_id` varchar(48) NOT NULL,
+  `project_id` varchar(48) DEFAULT NULL,
+  `extract_id` varchar(48) DEFAULT NULL,
+  `pid` varchar(64) DEFAULT NULL,
+  `data` json DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`piece_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `projects`
@@ -118,28 +142,18 @@ DROP TABLE IF EXISTS `projects`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `projects` (
   `project_id` varchar(48) NOT NULL,
-  `owner_uid` varchar(64) NOT NULL,
-  `keystr` varchar(64) NOT NULL DEFAULT 'KEY_STR',
-  `title` text NOT NULL,
+  `keystr` varchar(64) DEFAULT NULL,
+  `owner_uid` varchar(64) DEFAULT NULL,
+  `title` text,
   `abstract` text,
-  `date_created` datetime NOT NULL,
-  `date_updated` datetime NOT NULL,
-  `settings` json DEFAULT NULL COMMENT 'Store the collector setting and other configs for this project',
-  `is_deleted` varchar(8) NOT NULL DEFAULT 'no',
+  `date_created` datetime DEFAULT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  `settings` json DEFAULT NULL,
+  `is_deleted` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`project_id`),
-  UNIQUE KEY `key_str_UNIQUE` (`keystr`)
+  UNIQUE KEY `keystr` (`keystr`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `projects`
---
-
-LOCK TABLES `projects` WRITE;
-/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES ('0b8098bc-99e9-11ea-9a4b-98eecba7646f','hehuan2112@gmail.com','0B8098BD','The small lung cell project','The small lung cell project','2020-05-19 10:54:39','2020-05-19 10:54:39','{\"collect_template\": {}}','no'),('954af104-99ea-11ea-9a4b-98eecba7646f','hehuan2112@gmail.com','954AF105','The kidney cancer project','The kidney','2020-05-19 11:05:39','2020-05-19 11:05:39','{\"collect_template\": {}}','no');
-/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `rel_project_users`
@@ -150,20 +164,13 @@ DROP TABLE IF EXISTS `rel_project_users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rel_project_users` (
   `project_id` varchar(48) NOT NULL,
-  `uid` varchar(64) NOT NULL,
-  PRIMARY KEY (`project_id`,`uid`)
+  `uid` varchar(48) NOT NULL,
+  PRIMARY KEY (`project_id`,`uid`),
+  KEY `uid` (`uid`),
+  CONSTRAINT `rel_project_users_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`),
+  CONSTRAINT `rel_project_users_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `rel_project_users`
---
-
-LOCK TABLES `rel_project_users` WRITE;
-/*!40000 ALTER TABLE `rel_project_users` DISABLE KEYS */;
-INSERT INTO `rel_project_users` VALUES ('0b8098bc-99e9-11ea-9a4b-98eecba7646f','hehuan2112@gmail.com'),('954af104-99ea-11ea-9a4b-98eecba7646f','hehuan2112@gmail.com');
-/*!40000 ALTER TABLE `rel_project_users` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -177,21 +184,11 @@ CREATE TABLE `users` (
   `password` varchar(128) DEFAULT NULL,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
-  `role` varchar(16) NOT NULL DEFAULT 'user',
-  `is_deleted` varchar(8) NOT NULL DEFAULT 'no',
+  `role` varchar(16),
+  `is_deleted` varchar(8),
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('hehuan2112@gmail.com','pbkdf2:sha256:150000$oHLnajng$9be224b50b962de1d83cd745124fc6fe6828e5079c560fa6654d668439b77503','Huan','He','user','no'),('sipra.irbaz@gmail.com','pbkdf2:sha256:150000$oHLnajng$9be224b50b962de1d83cd745124fc6fe6828e5079c560fa6654d668439b77503','Irbaz','Riaz','user','no');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -202,4 +199,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-19 15:13:19
+-- Dump completed on 2023-05-06  3:07:45
